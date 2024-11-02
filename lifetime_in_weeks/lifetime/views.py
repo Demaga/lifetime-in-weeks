ddd35cb1c14dc1edcb295dfd319fda0a7be69217
Bohdan Kholodenko <bohdan.kholodenko@gmail.com>
2024-11-02 10:48:58 +0200

    Add index.html page

--- lifetime_in_weeks/lifetime/views.py
+++ lifetime_in_weeks/lifetime/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import render
 
-# Create your views here.
+from .models import Lifetime
+
+
+def index(request):
+    lifetimes = Lifetime.objects.order_by("-birth_date")[:5]
+    context = {"lifetimes": lifetimes}
+    return render(request, "lifetime/index.html", context)
